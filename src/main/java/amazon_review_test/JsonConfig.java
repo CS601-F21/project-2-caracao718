c81378784c9442d732ccac51fa620b9d20f006d8
Cara Cao <caracaoengineer@gmail.com>
2021-10-13 16:29:16 -0700

    added documentation

--- src/main/java/amazon_review_test/JsonConfig.java
+++ src/main/java/amazon_review_test/JsonConfig.java
@@ -1,5 +1,8 @@
 package amazon_review_test;
 
+/**
+ * A class that configure all the inputs needed for the program from the command line
+ */
 public class JsonConfig {
     private final String fileName1;
     private final String fileName2;
@@ -7,6 +10,14 @@ public class JsonConfig {
     private final String output2;
     private final String brokerName;
 
+    /**
+     * The constructor to instantiate the JsonConfiguration
+     * @param fileName1
+     * @param fileName2
+     * @param output1
+     * @param output2
+     * @param brokerName
+     */
     public JsonConfig(String fileName1, String fileName2, String output1, String output2, String brokerName) {
         this.fileName1 = fileName1;
         this.fileName2 = fileName2;
@@ -15,22 +26,42 @@ public class JsonConfig {
         this.brokerName = brokerName;
     }
 
+    /**
+     * A getter to retrieve the brokerName
+     * @return
+     */
     public String getBrokerName() {
         return brokerName;
     }
 
+    /**
+     * A getter to retrieve the first filename
+     * @return
+     */
     public String getFileName1() {
         return fileName1;
     }
 
+    /**
+     * A getter to retrieve the second filename
+     * @return
+     */
     public String getFileName2() {
         return fileName2;
     }
 
+    /**
+     * A getter to retrieve the first output filename
+     * @return
+     */
     public String getOutput1() {
         return output1;
     }
 
+    /**
+     * A getter to retrieve the second output filename
+     * @return
+     */
     public String getOutput2() {
         return output2;
     }
--- src/main/java/framework/BrokerFactory.java
+++ src/main/java/framework/BrokerFactory.java
@@ -1,7 +1,15 @@
 package framework;
 
+/**
+ * A class that is used for the Factory Design Pattern
+ */
 public class BrokerFactory {
 
+    /**
+     * A method that takes the string of broker name and return a Broker object
+     * @param brokerName
+     * @return
+     */
     public Broker getBroker(String brokerName) {
         if (brokerName.equals("SynchronousOrderedDispatchBroker")) {
             return new SynchronousOrderedDispatchBroker();
